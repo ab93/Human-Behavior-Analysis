de562f2e0bc1e3c4811167ec51fb44f06d7bebb1
ab93 <avikbasu93@gmail.com>
2016-10-10 16:08:06 -0700

    New plots and added stat scores

--- src/plot.py
+++ src/plot.py
@@ -12,9 +12,44 @@ def get_data(feature_file, feature_name):
     feat_df = feat_df[feature_name]
     return feat_df, excel_df
 
+
+def plot_histogram(x, y, feature_name):
+    classes = np.unique(y.values)[:-1]
+    classes = map(np.int,classes)
+
+    z = pd.concat([x,y], axis=1)
+    z = z.replace([np.inf, -np.inf], np.nan)
+    z = z.dropna()
+    z = z.groupby(by='majority vote')
+
+    data = []
+    for key, item in z:
+        temp_df = z.get_group(key)
+        data.append(temp_df.iloc[:,0].values)
+
+    plt.hist(data[2],normed=True)
+    plt.show()
+
+
+def calculate_stat_scores(x,y,feature_name):
+    z = pd.concat([x,y], axis=1)
+    z = z.replace([np.inf, -np.inf], np.nan)
+    z = z.dropna()
+    z = z.groupby(by='majority vote')
+
+    data = []
+    for key, item in z:
+        temp_df = z.get_group(key)
+        data.append(temp_df.iloc[:,0].values)
+
+    with open('../results/stats.dat', 'a+') as f:
+        f.write(feature_name + '\t')
+        for class_data in data:
+            f.write(str(np.mean(class_data)) + '\t' + str(np.median(class_data)) + '\t' + str(np.std(class_data)) + '\t')
+        f.write('\n')
+
 def calculate_anova(x,y,feature_name):
     classes = np.unique(y.values)[:-1]
-    print classes
     classes = map(np.int,classes)
 
     z = pd.concat([x,y], axis=1)
@@ -29,13 +64,12 @@ def calculate_anova(x,y,feature_name):
 
     f_val, p_val = stats.f_oneway(data[0], data[1], data[2])
     print p_val
-    with open('../results/anova.dat', 'a+') as f:
+    with open('../results/anova_std.dat', 'a+') as f:
         f.write(feature_name + '\t' + str(p_val) + '\n')
 
 
 def plot_box(x,y,feature_name):
     classes = np.unique(y.values)[:-1]
-    print classes
     classes = map(np.int,classes)
 
     z = pd.concat([x,y], axis=1)
@@ -51,25 +85,34 @@ def plot_box(x,y,feature_name):
 
     fig = plt.figure(1, figsize=(9, 6))
     ax = fig.add_subplot(111)
-    bp = ax.boxplot(data)
-    ax.set_xticklabels(classes)
-    ax.set_ylabel(feature_name)
-    ax.set_xlabel('Majority Vote')
+    bp = ax.boxplot(data, notch=True, sym='+', vert=True, whis=1.5, patch_artist=True)
+    ax.set_xticklabels(['Negative','Neutral','Positive'])
+    #ax.set_ylabel(feature_name)
+    ax.set_ylabel('Smile Level')
+    ax.set_xlabel('Class Label (Majority Vote)')
 
-    for box in bp['boxes']:
-        box.set(color='red', linewidth=2)
+    plt.grid(axis='y',          # set y-axis grid lines
+        linestyle='--',     # use dashed lines
+        which='major',      # only major ticks
+        color='black',  # line colour
+        alpha=0.25)
+
+    colors = ['red', 'blue', 'green']
+    for box,color in zip(bp['boxes'],colors):
+        box.set(color='black', linewidth=0.5)
+        box.set_facecolor(color)
 
     for whisker in bp['whiskers']:
-        whisker.set(color='black', linewidth=2)
+        whisker.set(color='grey', linewidth=2)
 
     for cap in bp['caps']:
         cap.set(color='black', linewidth=2)
 
     for median in bp['medians']:
-        median.set(color='blue', linewidth=2)
+        median.set(color='black', linewidth=3)
 
     for flier in bp['fliers']:
-        flier.set(marker='o', color='#e7298a', alpha=0.5)
+        flier.set(marker='o', color='green', alpha=0.4)
     fig.savefig('../plots/' + feature_name + '.png')
 
 
@@ -78,4 +121,6 @@ if __name__ == '__main__':
     feature_file = sys.argv[1]
     feat_df, excel_df = get_data(feature_file, feature_name)
     plot_box(feat_df, excel_df, feature_name)
-    calculate_anova(feat_df, excel_df, feature_name)
+    #calculate_anova(feat_df, excel_df, feature_name)
+    #plot_histogram(feat_df, excel_df, feature_name)
+    calculate_stat_scores(feat_df, excel_df, feature_name)
