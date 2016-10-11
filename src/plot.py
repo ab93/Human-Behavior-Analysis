be841c02460b7764871c91583c315e1ec104456d
Madhusudhan Krishnamachari <madhusudhankrishnamachari@gmail.com>
2016-10-10 17:33:13 -0700

    Added t-test code

--- src/plot.py
+++ src/plot.py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sys
+import csv
 from scipy import stats
 
 def get_data(feature_file, feature_name):
-    excel_df = pd.read_excel(open('../../Assignment_1/sentimentAnnotations_rev_v03.xlsx','rb'),
+    excel_df = pd.read_excel(open('../sentimentAnnotations_rev_v03.xlsx','rb'),
                             sheetname='Sheet1')
     excel_df = excel_df['majority vote']
     feat_df = pd.read_csv(feature_file)
     feat_df = feat_df[feature_name]
     return feat_df, excel_df
-
 
 def plot_histogram(x, y, feature_name):
     classes = np.unique(y.values)[:-1]
@@ -29,7 +29,6 @@ def plot_histogram(x, y, feature_name):
 
     plt.hist(data[2],normed=True)
     plt.show()
-
 
 def calculate_stat_scores(x,y,feature_name):
     z = pd.concat([x,y], axis=1)
@@ -66,6 +65,29 @@ def calculate_anova(x,y,feature_name):
     print p_val
     with open('../results/anova_std.dat', 'a+') as f:
         f.write(feature_name + '\t' + str(p_val) + '\n')
+
+def calculate_t(x, y, feature_name):
+    classes = np.unique(y.values)[:-1]
+    classes = map(np.int,classes)
+
+    z = pd.concat([x,y], axis=1)
+    z = z.replace([np.inf, -np.inf], np.nan)
+    z = z.dropna()
+    z = z.groupby(by='majority vote')
+
+    data = []
+    for key, item in z:
+        temp_df = z.get_group(key)
+        data.append(temp_df.iloc[:,0].values)
+
+    ans_neg_neu=stats.ttest_ind(data[0], data[1], 0)
+    ans_neu_pos=stats.ttest_ind(data[1], data[2], 0)
+    ans_neg_pos=stats.ttest_ind(data[0], data[2], 0)
+
+    print ans_neg_neu, ans_neu_pos, ans_neg_pos
+    with open('../results/t_test_results.dat','a') as f:
+        writer=csv.writer(f, delimiter='\t')
+        writer.writerow([feature_name, ans_neg_neu, ans_neu_pos, ans_neg_pos])
 
 
 def plot_box(x,y,feature_name):
@@ -120,7 +142,9 @@ if __name__ == '__main__':
     feature_name = sys.argv[2]
     feature_file = sys.argv[1]
     feat_df, excel_df = get_data(feature_file, feature_name)
-    plot_box(feat_df, excel_df, feature_name)
+    #plot_box(feat_df, excel_df, feature_name)
     #calculate_anova(feat_df, excel_df, feature_name)
     #plot_histogram(feat_df, excel_df, feature_name)
-    calculate_stat_scores(feat_df, excel_df, feature_name)
+    #calculate_stat_scores(feat_df, excel_df, feature_name)
+    calculate_t(feat_df, excel_df, feature_name)
+
