9af07159a8d22837fe5ad0e2f1a7a2f97c125e86
ab93 <avikbasu93@gmail.com>
2016-11-02 13:58:03 -0700

    Added get_values

--- src/classify.py
+++ src/classify.py
@@ -2,19 +2,26 @@ import numpy as np
 import pandas as pd
 from sklearn import svm, preprocessing
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.grid_search import GridSearchCV
 import matplotlib.pyplot as plt
+import random
 
 ANNOT_FILE_PATH = '../data/sentimentAnnotations_rev_v03.xlsx'
 ACOUSTIC_FILE_PATH = '../results/acou_mean.csv'
 VISUAL_FILE_PATH = '../results/okao_mean.csv'
+  
 
-def split_data(exp=3):
+def split_data(exp=0):
     df = pd.read_excel(ANNOT_FILE_PATH, sheetname='Sheet1')
+    grouped = df.groupby('video')
+    data = grouped.groups.items()
+    random.shuffle(data)
     split = [None] * 4
-    split[0] = np.arange(67)
-    split[1] = np.arange(67,140)
-    split[2] = np.arange(140,210)
-    split[3] = np.arange(210,280)
+    split[0] = [ item for sublist in [tuple_[1] for tuple_ in data[:12]] for item in sublist]
+    split[1] = [ item for sublist in [tuple_[1] for tuple_ in data[12:24]] for item in sublist]
+    split[2] = [ item for sublist in [tuple_[1] for tuple_ in data[24:36]] for item in sublist]
+    split[3] = [ item for sublist in [tuple_[1] for tuple_ in data[36:48]] for item in sublist]
+
     if exp == 0:
         return np.append(split[0],split[1]), split[2], split[3]
     elif exp == 1:
@@ -23,38 +30,65 @@ def split_data(exp=3):
         return np.append(split[2],split[3]), split[0], split[1]
     else:
         return np.append(split[1],split[2]), split[3], split[0]
-
+    
 
 def fuse_features():
     labels_df = pd.read_excel(ANNOT_FILE_PATH, sheetname='Sheet1')
     labels_df = labels_df['majority vote']
 
     acou_feat_df = pd.read_csv(ACOUSTIC_FILE_PATH)
-    #acou_feat_df = acou_feat_df[['naq','frequency','energy']]
+    acou_feat_df = acou_feat_df[['naq','frequency','energy']]
 
     acou_feat_df = acou_feat_df.replace([np.inf, -np.inf], np.nan)
     acou_feat_df = acou_feat_df.apply(lambda x: x.fillna(x.mean()),axis=0)
-    acou_feat_df = acou_feat_df['frequency']
 
     visual_feat_df = pd.read_csv(VISUAL_FILE_PATH)
-    #visual_feat_df = visual_feat_df[['face_up_down','mouth_open','smile_level']]
-    visual_feat_df = visual_feat_df[['mouth_open']]
+    visual_feat_df = visual_feat_df[['face_up_down','mouth_open','smile_level']]
 
     return pd.concat([acou_feat_df, visual_feat_df, labels_df], axis=1)
 
+def get_values(training_set,validation_set,test_set):
+    
+    #validation accuracy
+    params = {'C': [0.001,0.01,1,10,100]}
+    clf_validation = svm.SVC(decision_function_shape='ovr',kernel='linear')
+    grid_clf = GridSearchCV(estimator=clf_validation, param_grid=params, n_jobs=3)
+    grid_clf.fit(training_set[:,:-1], training_set[:,-1])
+    print grid_clf
+    validation_accuracy = grid_clf.score(validation_set[:,:-1],validation_set[:,-1])
+    print "validation accuracy is ", validation_accuracy
+    c = grid_clf.best_estimator_.C
+    print c
+
+    '''
+    #training accuracy
+    clf_training = svm.SVC(decision_function_shape='ovr',kernel='linear',C=c)
+    clf_training.fit(training_set[:,:-1], training_set[:,-1])
+    training_accuracy = clf_training.score(training_set[:,:-1], training_set[:,-1])
+    print training_accuracy
+
+    #test accuracy
+    testing_accuracy = clf_training.score(test_set[:,:-1],test_set[:,-1])
+    print testing_accuracy
+    '''
 
 def model():
     df = fuse_features()
     train_idx, val_idx, test_idx = split_data()
 
-    train_features = df.iloc[train_idx,:-1].values
-    train_labels = df.iloc[train_idx,-1].values
+    train_features = df.iloc[train_idx,:-1]
+    train_labels = df.iloc[train_idx,-1]
+    training_set = pd.concat([train_features, train_labels], axis=1)
 
-    val_features = df.iloc[val_idx,:-1].values
-    val_labels = df.iloc[val_idx,-1].values
+    val_features = df.iloc[val_idx,:-1]
+    val_labels = df.iloc[val_idx,-1]
+    validaition_set = pd.concat([val_features, val_labels], axis=1)
 
-    test_features = df.iloc[test_idx,:-1].values
-    test_labels = df.iloc[test_idx,-1].values
+    test_features = df.iloc[test_idx,:-1]
+    test_labels = df.iloc[test_idx,-1]
+    test_set = pd.concat([test_features, test_labels], axis=1)
+
+    get_values(training_set.values, validaition_set.values, test_set.values)
 
     #scaler = preprocessing.StandardScaler().fit(train_features)
     #x_train = scaler.transform(train_features)
@@ -62,16 +96,22 @@ def model():
     #print train_features[:,0].shape
     #raw_input()
 
+    
+
+
+    '''
     plt.figure(2, figsize=(8, 6))
     plt.scatter(train_features[:, 0], train_features[:, 1], c=train_labels, cmap=plt.cm.Paired)
     plt.xlabel('naq')
     plt.ylabel('mouth_open')
+    
 
     #plt.xlim(x_min, x_max)
     #plt.ylim(y_min, y_max)
     plt.xticks(())
     plt.yticks(())
     #plt.show()
+    
 
     clf = svm.SVC(C=1)
     #clf = svm.LinearSVC(C=0.1, dual=False, multi_class='ovr',)
@@ -82,6 +122,11 @@ def model():
     #print train_labels
 
     print clf.score(val_features, val_labels)
+    '''
+
+
+
+
 
 
 if __name__ == '__main__':
