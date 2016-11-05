360aa4424bf5f305e5fc4d43ae359d7df2c0cd1e
Indhu Kamala Kumar <Indhu@guest-wireless-207-151-057-078.usc.edu>
2016-11-04 17:06:31 -0700

    added plots for exp 2 and Gaussian NB

--- src/classify.py
+++ src/classify.py
@@ -6,11 +6,14 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.model_selection import validation_curve
 import matplotlib.pyplot as plt
 import random
+import scipy
+from sklearn.naive_bayes import GaussianNB
 
 ANNOT_FILE_PATH = '../data/sentimentAnnotations_rev_v03.xlsx'
 ACOUSTIC_FILE_PATH = '../results/acou_mean.csv'
 VISUAL_FILE_PATH = '../results/okao_mean.csv'
 SHORE_FILE_PATH = '../results/shore_mean.csv'
+split = [None] * 4
   
 def plot_validation_curve(X, y, clf, cv, param_name, param_range, title=''):
     train_scores, test_scores = validation_curve(clf, X, y, 
@@ -41,17 +44,18 @@ def plot_validation_curve(X, y, clf, cv, param_name, param_range, title=''):
     #plt.show()
     fig.savefig('../plots/val_curve/VC_'+title+'.png')
 
-def split_data(exp=1):
+def randomize():
     df = pd.read_excel(ANNOT_FILE_PATH, sheetname='Sheet1')
     grouped = df.groupby('video')
     data = grouped.groups.items()
     random.shuffle(data)
-    split = [None] * 4
-    split[0] = [ item for sublist in [tuple_[1] for tuple_ in data[:12]] for item in sublist]
-    split[1] = [ item for sublist in [tuple_[1] for tuple_ in data[12:24]] for item in sublist]
-    split[2] = [ item for sublist in [tuple_[1] for tuple_ in data[24:36]] for item in sublist]
-    split[3] = [ item for sublist in [tuple_[1] for tuple_ in data[36:48]] for item in sublist]
 
+    split[0] = [item for sublist in [tuple_[1] for tuple_ in data[:12]] for item in sublist]
+    split[1] = [item for sublist in [tuple_[1] for tuple_ in data[12:24]] for item in sublist]
+    split[2] = [item for sublist in [tuple_[1] for tuple_ in data[24:36]] for item in sublist]
+    split[3] = [item for sublist in [tuple_[1] for tuple_ in data[36:48]] for item in sublist]
+
+def split_data(exp=1):
     if exp == 0:
         return np.append(split[0],split[1]), split[2], split[3]
     elif exp == 1:
@@ -61,7 +65,6 @@ def split_data(exp=1):
     else:
         return np.append(split[1],split[2]), split[3], split[0]
     
-
 def fuse_features():
     labels_df = pd.read_excel(ANNOT_FILE_PATH, sheetname='Sheet1')
     labels_df = labels_df['majority vote']
@@ -117,7 +120,6 @@ def get_visual_featues():
 
     return pd.concat([visual_feat_df, shore_feat_df, labels_df], axis=1)
 
-
 def get_values(training_set,validation_set,test_set, exp_no, val='hold', VC=True):
     
     # find value of C
@@ -157,10 +159,10 @@ def get_values(training_set,validation_set,test_set, exp_no, val='hold', VC=True
     
     grid_clf.fit(data,labels)
     validation_accuracy = grid_clf.best_score_
-    print "validation_accuracy:", validation_accuracy
+
     c = grid_clf.best_estimator_.C
     print "C:",c
-
+    print "validation_accuracy:", validation_accuracy
     #train accuracy
     
     best_clf = svm.LinearSVC(C=c, dual=False)
@@ -173,9 +175,9 @@ def get_values(training_set,validation_set,test_set, exp_no, val='hold', VC=True
     
     testing_accuracy = best_clf.score(testing_data,testing_labels)
     print "testing accuracy:", testing_accuracy
-    
-    return (training_accuracy, validation_accuracy, testing_accuracy)
-
+    test_predictions = best_clf.predict(testing_data)
+    accuracies = [training_accuracy, validation_accuracy, testing_accuracy]
+    return accuracies, test_predictions
 
 def get_svm_rbf_values(training_set,validation_set,test_set):
     training_data = training_set[:,:-1]
@@ -194,9 +196,9 @@ def get_svm_rbf_values(training_set,validation_set,test_set):
     validation_indices = range(len(training_data),len(validation_data)+len(training_data))
 
     clf_rf = svm.SVC()
-    
+
     grid_clf = GridSearchCV(estimator=clf_rf, n_jobs=-1, param_grid=param_grid,cv=[(train_indices,validation_indices)])
-        
+
     grid_clf.fit(data,labels)
     print "\nSVM RBF:"
     print grid_clf.best_params_
@@ -204,7 +206,7 @@ def get_svm_rbf_values(training_set,validation_set,test_set):
     print "validation_accuracy:", validation_accuracy
 
     #train accuracy
-    
+
     best_clf = grid_clf.best_estimator_
     best_clf.fit(data,labels)
     training_accuracy = best_clf.score(data,labels)
@@ -214,7 +216,6 @@ def get_svm_rbf_values(training_set,validation_set,test_set):
     testing_accuracy = best_clf.score(testing_data,testing_labels)
     print "testing accuracy:", testing_accuracy
     return (training_accuracy, validation_accuracy, testing_accuracy)
-
 
 def get_rf_values(training_set,validation_set,test_set):
     training_data = training_set[:,:-1]
@@ -237,9 +238,9 @@ def get_rf_values(training_set,validation_set,test_set):
     validation_indices = range(len(training_data),len(validation_data)+len(training_data))
 
     clf_rf = RandomForestClassifier()
-    
+
     grid_clf = GridSearchCV(estimator=clf_rf, n_jobs=-1, param_grid=param_grid,cv=[(train_indices,validation_indices)])
-        
+
     grid_clf.fit(data,labels)
     print "\nRandom Forests:"
     print grid_clf.best_params_
@@ -247,7 +248,7 @@ def get_rf_values(training_set,validation_set,test_set):
     print "validation_accuracy:", validation_accuracy
 
     #train accuracy
-    
+
     best_clf = grid_clf.best_estimator_
     best_clf.fit(data,labels)
     training_accuracy = best_clf.score(data,labels)
@@ -274,14 +275,113 @@ def plot_val_test(scores, title, filename):
     plt.plot(np.arange(1,5), test_scores, 'o-', label="Test Scores", color='blue')
     plt.legend(loc='best')
     fig.savefig('../plots/val_test/' + filename + '.png')
-    
-    
+
+def draw_multi_plots(multi_accuracy_list,acou_accuracy_list,visual_accuracy_list):
+
+    acou_val_scores = [score[1] for score in acou_accuracy_list]
+    acou_test_scores = [score[2] for score in acou_accuracy_list]
+    visual_val_scores = [score[1] for score in visual_accuracy_list]
+    visual_test_scores = [score[2] for score in visual_accuracy_list]
+    multi_val_scores = [score[1] for score in multi_accuracy_list]
+    multi_test_scores = [score[2] for score in multi_accuracy_list]
+
+    plt.clf()
+    plt.close()
+    fig = plt.figure()
+    plt.title("Acoustic Only")
+    plt.xlabel("Test Folds ID")
+    plt.ylabel("Accuracy")
+    plt.ylim(0.0, 1.1)
+    plt.xlim(0, 5)
+    plt.xticks(np.arange(0, 6))
+    plt.plot(np.arange(1, 5), acou_val_scores, 'o-', label="Acoustic Validation Scores", color='red')
+    plt.plot(np.arange(1, 5), acou_test_scores, 'o-', label="Acoustic Test Scores", color='blue')
+    plt.legend(loc='best')
+    #plt.axis([0, 5, 0,1])
+    fig.savefig('../plots/val_test/' + "acoustic_only" + '.png')
+
+    plt.clf()
+    plt.close()
+    fig = plt.figure()
+    plt.title("Visual Only")
+    plt.xlabel("Test Folds ID")
+    plt.ylabel("Accuracy")
+    plt.ylim(0.0, 1.1)
+    plt.xlim(0, 5)
+    plt.xticks(np.arange(0, 6))
+    plt.plot(np.arange(1, 5), visual_val_scores, 'o-', label="Visual Validation Scores", color='red')
+    plt.plot(np.arange(1, 5), visual_test_scores, 'o-', label="Visual Test Scores", color='blue')
+    plt.legend(loc='best')
+    # plt.axis([0, 5, 0,1])
+    fig.savefig('../plots/val_test/' + "visual_only" + '.png')
+
+    plt.clf()
+    plt.close()
+    fig = plt.figure()
+    plt.title("Acoustic vs Multimodal")
+    plt.xlabel("Test Folds ID")
+    plt.ylabel("Accuracy")
+    plt.ylim(0.0, 1.1)
+    plt.xlim(0, 5)
+    plt.xticks(np.arange(0, 6))
+    plt.plot(np.arange(1, 5), acou_test_scores, 'o-', label="Acoustic Test Scores", color='red')
+    plt.plot(np.arange(1, 5), multi_test_scores, 'o-', label="Multimodal Test Scores", color='blue')
+    plt.legend(loc='best')
+    # plt.axis([0, 5, 0,1])
+
+    fig.savefig('../plots/val_test/' + "acoustic+multi" + '.png')
+
+    plt.clf()
+    plt.close()
+    fig = plt.figure()
+    plt.title("Visual vs Multimodal")
+    plt.xlabel("Test Folds ID")
+    plt.ylabel("Accuracy")
+    plt.ylim(0.0, 1.1)
+    plt.xlim(0, 5)
+    plt.xticks(np.arange(0, 6))
+    plt.plot(np.arange(1, 5), visual_test_scores, 'o-', label="Visual Test Scores", color='red')
+    plt.plot(np.arange(1, 5), multi_test_scores, 'o-', label="Multimodal Test Scores", color='blue')
+    plt.legend(loc='best')
+    # plt.axis([0, 5, 0,1])
+    fig.savefig('../plots/val_test/' + "visual+multi" + '.png')
+
+    plt.clf()
+    plt.close()
+    fig = plt.figure()
+    plt.title("Visual vs Acoustic")
+    plt.xlabel("Test Folds ID")
+    plt.ylabel("Accuracy")
+    plt.ylim(0.0, 1.1)
+    plt.xlim(0, 5)
+    plt.xticks(np.arange(0, 6))
+    plt.plot(np.arange(1, 5), visual_test_scores, 'o-', label="Visual Test Scores", color='red')
+    plt.plot(np.arange(1, 5), acou_test_scores, 'o-', label="Acoustic Test Scores", color='blue')
+    plt.legend(loc='best')
+    # plt.axis([0, 5, 0,1])
+    fig.savefig('../plots/val_test/' + "visual+acoustic" + '.png')
+
+def Gaussian_NB(training_set,test_set):
+    training_data = training_set[:, :-1]
+    training_labels = training_set[:, -1]
+    testing_data = test_set[:, :-1]
+    testing_labels = test_set[:, -1]
+    clf = GaussianNB()
+    clf.fit(training_data, training_labels)
+    testing_accuracy = clf.score(testing_data,testing_labels)
+    test_predictions = clf.predict(testing_data)
+
+    return testing_accuracy,test_predictions
+
 
 def model(exp):
     hold_out_scores = []
     fold_scores = []
     rbf_scores = []
     rf_scores = []
+    predictions = []
+    gaussian_values = []
+    gaussian_predictions = []
 
     if exp == "Exp 1":
         df = fuse_features()
@@ -307,37 +407,48 @@ def model(exp):
         test_set = pd.concat([test_features, test_labels], axis=1)
 
         print "\nHold-out:"
-        hold_out_scores.append(get_values(training_set.values, validaition_set.values, test_set.values, 
-                                str(i+1), val='hold'))
+        a,p = get_values(training_set.values, validaition_set.values, test_set.values,
+                                str(i+1), val='hold')
+        hold_out_scores.append(a)
+        predictions.append(p)
+        temp_a,temp_p = Gaussian_NB(training_set.values,test_set.values)
+        gaussian_values.append(temp_a)
+        gaussian_predictions.append(temp_p)
+
 
         rf_scores.append(get_rf_values(training_set.values, validaition_set.values, test_set.values))
-        #rbf_scores.append(get_values(training_set.values, validaition_set.values, test_set.values, 
-        #                        str(i+1), val='hold', VC=False))
-
         rbf_scores.append(get_svm_rbf_values(training_set.values, validaition_set.values, test_set.values))
 
         if exp == "Exp 1":
             print "\n3-Fold:"
-            fold_scores.append(get_values(training_set.values, validaition_set.values, test_set.values,
-                             str(i+1), val='3-fold'))
+            a,p = get_values(training_set.values, validaition_set.values, test_set.values,str(i+1), val='3-fold')
+            fold_scores.append(a)
+    if exp == "Exp 1":
+        plot_val_test(fold_scores, "3 fold scores", "3_fold")
+        print gaussian_values
+        print t_test_values(np.concatenate(hold_out_scores).ravel().tolist(),
+                      np.concatenate(gaussian_predictions).ravel().tolist())
 
-    plot_val_test(hold_out_scores, "Hold out scores", "hold_out")
-    plot_val_test(fold_scores, "3 fold scores", "3_fold")
-    return hold_out_scores
+        raw_input()
 
 
-def draw_multi_plots(acou_accuracy_list,visual_accuracy_list):
-    print acou_accuracy_list
-    print visual_accuracy_list
-    plt.plot([1,2,3,4],acou_accuracy_list, 'r-')
-    plt.plot([1,2,3,4],visual_accuracy_list,'b-')
-    plt.axis([0, 5, 0,1])
-    plt.show()
+    return hold_out_scores,predictions
 
+def t_test_values(prediction_1,prediction_2):
+    t_value = scipy.stats.ttest_ind(prediction_1,prediction_2,None ,True)
+    return t_value
 
 if __name__ == '__main__':
-    model("Exp 1")
-    #acoustic = model("Exp 2 a")
-    #visual = model("Exp 2 b")
-    #draw_multi_plots(acoustic,visual)
+    randomize()
+    print "Experiment A"
+    multi_accuracies,multimodal_predictions = model("Exp 1")
+    plot_val_test(multi_accuracies, "Hold out scores", "hold_out")
+    print "Experiment 2 a"
+    acoustic_accuracies,acoustic_predictions = model("Exp 2 a")
+    print "Experiment 2 b"
+    visual_accuracies,visual_predictions = model("Exp 2 b")
+    draw_multi_plots(multi_accuracies,acoustic_accuracies,visual_accuracies)
+    print "t scores"
+    print t_test_values(np.concatenate(multimodal_predictions).ravel().tolist(), np.concatenate(acoustic_predictions).ravel().tolist())
+    print t_test_values(np.concatenate(multimodal_predictions).ravel().tolist(),np.concatenate(visual_predictions).ravel().tolist())
 
